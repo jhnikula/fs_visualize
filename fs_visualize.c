ecc23a5cbbe87f7f383dffda53a6901ed4659d42
Jarkko Nikula <jarkko.nikula@bitmer.com>
2013-09-16 20:50:46 +0300

    Remove needless NULL check before free()
    free() allows also NULL argument.
    
    Signed-off-by: Jarkko Nikula <jarkko.nikula@bitmer.com>

--- fs_visualize.c
+++ fs_visualize.c
@@ -106,7 +106,7 @@ finalise:
 	if (fp != NULL) fclose(fp);
 	if (info_ptr != NULL) png_free_data(png_ptr, info_ptr, PNG_FREE_ALL, -1);
 	if (png_ptr != NULL) png_destroy_write_struct(&png_ptr, (png_infopp)NULL);
-	if (row != NULL) free(row);
+	free(row);
 
 	return code;
 }
