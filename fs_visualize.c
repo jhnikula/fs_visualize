e5ccc96f0f2de2b80308d17ae19e2ca6563c2662
Jarkko Nikula <jarkko.nikula@bitmer.com>
2015-01-16 22:15:33 +0200

    Replace __builtin_cpu_supports() with __get_cpuid()
    __builtin_cpu_init() and __builtin_cpu_supports() are available only since
    gcc 4.8. Replace these with more lower level __get_cpuid() because current
    Debian/stable 7.x is using gcc 4.7.
    
    Signed-off-by: Jarkko Nikula <jarkko.nikula@bitmer.com>

--- fs_visualize.c
+++ fs_visualize.c
@@ -135,6 +135,9 @@ int main(int argc, char **argv)
 	time_t t;
 	char outf_name[MAX_FILE_NAME];
 	unsigned char (*avg)(unsigned char *buf, int len) = avg_generic;
+#if defined(__i386__) || defined(__x86_64__)
+	unsigned int eax, ebx, ecx, edx = 0;
+#endif
 
 	if (argc < 2 || argc > 3) {
 		printf("Usage: %s <source_file> [<target_file>]\n", argv[0]);
@@ -167,8 +170,8 @@ int main(int argc, char **argv)
 	}
 
 #if defined(__i386__) || defined(__x86_64__)
-	__builtin_cpu_init();
-	if (__builtin_cpu_supports("sse2")) {
+	__get_cpuid(1, &eax, &ebx, &ecx, &edx);
+	if (edx & bit_SSE2) {
 		printf("Using SSE2 optimizations\n");
 		avg = avg_sse2;
 	}
